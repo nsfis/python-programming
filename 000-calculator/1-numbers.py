66b7209fd6262e2cf2db02f0c1e357963f281dc7
nsfis <fis.itsfun@gmail.com>
2025-10-19 16:44:23 +0700

    Add files via upload

---
+++ 000-calculator/1-numbers.py
@@ -0,0 +1,24 @@
+# Numbers
+print(2 + 2)
+print(50 - 5 * 6)
+print((50 - 5 * 6) / 4)
+print(8 / 5) # pembagian selalu mengembalikan nilai float
+
+print(17 / 3)
+print(17 // 3) # floor division
+print(17 % 3) # sisa pembagian
+print(5 * 3 + 2)
+
+print(5 ** 2) # perpangkatan
+print(2 ** 7)
+
+width = 20
+height = 5 * 9
+print(width * height)
+
+print(4 * 3.75 - 1)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
+
